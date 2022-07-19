2f722d0957e3345c9b7b25169483c2c33cea52be
ferpmdev <ferpm.dev@gmail.com>
2022-07-19 10:30:17 -0300

    se creo la 1er ruta

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 app.set("port", process.env.PORT || 9000);
 
+/////////////////////////////////////////////////////////
+// PRIMER RUTA /////////////
+/////////////////////////////////////////////////////////
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my API!!!!!!!");
+});
 app.listen(app.get("port"), () => {
   console.log("server runnig on port 9000!!!!!!!!!", app.get("port"));
 });
